06dc278975ec383c991d791f3044c1d846974dc9
Vivek <vivek.ratnaparkhi10@gmail.com>
2019-02-13 23:03:01 +0530

    reverse the number

--- 04_reverse_the_number.py
+++ 04_reverse_the_number.py
@@ -7,9 +7,13 @@ def reverse_number(num):
 
     return rev
 
-
-
 num = 123043
 num_rev = reverse_number(num)
 
 print(num_rev)
+
+# You can reverse the number by using Python language features. However, this is not the right way. For example, see below code.
+# We can call it language hack.
+
+# Convert the number in string and reverse it
+print(str(num)[::-1])
