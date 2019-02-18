7c4a75c831d6e94f00506e117d0c58d5a007e6c4
Vivek <vivek.ratnaparkhi10@gmail.com>
2019-02-18 23:35:03 +0530

    binary search tree

--- 05_binary_search_tree.py
+++ 05_binary_search_tree.py
@@ -5,7 +5,7 @@ class BinarySearchTree(object):
         self.right = None
 
     def add(self, item):
-        if item <= self.root:
+        if item < self.root:
             if self.left is None:
                 self.left = BinarySearchTree(item)
             else:
@@ -17,4 +17,18 @@ class BinarySearchTree(object):
                 self.right.add(item)
 
     def print_in_order(self):
-        pass
+        if self.left:
+            self.left.print_in_order()
+        print(self.root)
+        if self.right:
+            self.right.print_in_order()
+
+bst = BinarySearchTree(5)
+bst.add(8)
+bst.add(7)
+bst.add(1)
+bst.add(3)
+bst.add(9)
+bst.add(2)
+
+bst.print_in_order()
