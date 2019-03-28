dd76159f3b2d1563dad215af3a6078a5b9af8230
Vivek <vivek.ratnaparkhi10@gmail.com>
2019-03-28 22:55:27 +0530

    find largest other approach

--- 08_find_largest_ele_no_loop.py
+++ 08_find_largest_ele_no_loop.py
@@ -6,7 +6,7 @@ def find_largest(array, i):
         return array[i]
     else:
         ele = find_largest(array, i+1)
-
+    
     if ele > array[i]:
         return ele
     else:
@@ -19,8 +19,29 @@ input_3 = [1, 2, 5, 4, 3]
 input_4 = [3, 5]
 input_5 = [5, 3]
 
-assert find_largest(input_1, -1) == 5
-assert find_largest(input_2, -1) == 5
-assert find_largest(input_3, -1) == 5
-assert find_largest(input_4, -1) == 5
-assert find_largest(input_5, -1) == 5
+assert find_largest(input_1, 0) == 5
+assert find_largest(input_2, 0) == 5
+assert find_largest(input_3, 0) == 5
+assert find_largest(input_4, 0) == 5
+assert find_largest(input_5, 0) == 5
+
+# Approach#2
+def find_largest_2(array, i, large):
+    if i == len(array)-1:
+        if array[i] > large:
+            return array[i]
+        else:
+            return large
+
+    if array[i] > large:
+        ele = find_largest_2(array, i+1, array[i])
+    else:
+        ele = find_largest_2(array, i+1, large)
+
+    return ele
+
+assert find_largest_2(input_1, 0, 0) == 5
+assert find_largest_2(input_2, 0, 0) == 5
+assert find_largest_2(input_3, 0, 0) == 5
+assert find_largest_2(input_4, 0, 0) == 5
+assert find_largest_2(input_5, 0, 0) == 5
