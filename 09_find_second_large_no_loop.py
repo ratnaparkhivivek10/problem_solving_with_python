220ec7b2a93b049e08b685f2f590db93d2d16458
Vivek <vivek.ratnaparkhi10@gmail.com>
2019-03-28 21:56:42 +0530

    negative index not needed

---
+++ 09_find_second_large_no_loop.py
@@ -0,0 +1,15 @@
+def find_largest(array, i, large):
+    pass
+
+
+input_1 = [1, 2, 3, 4, 5]
+input_2 = [5, 2, 3, 4, 1]
+input_3 = [1, 2, 5, 4, 3]
+input_4 = [3, 5]
+input_5 = [5, 3]
+
+assert find_largest(input_1, -1, 0) == 5
+assert find_largest(input_2, -1, 0) == 5
+assert find_largest(input_3, -1, 0) == 5
+assert find_largest(input_4, -1, 0) == 5
+assert find_largest(input_5, -1, 0) == 5
