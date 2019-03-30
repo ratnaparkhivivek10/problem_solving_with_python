bdbfd04094ee423902ae5873bcbca6890884cfa9
Vivek <vivek.ratnaparkhi10@gmail.com>
2019-03-31 00:39:12 +0530

    find second largest element - using loop

--- 09_find_second_large_no_loop.py
+++ 09_find_second_large_no_loop.py
@@ -1,6 +1,15 @@
-def find_largest(array, i):
-   pass
-    
+def find_second_largest(array):
+   max_ = 0 
+   second_max = 0
+   for item in array:
+      if item > max_:
+         second_max = max_
+         max_ = item
+      elif item > second_max:
+         second_max = item
+
+   return second_max
+
 
 input_1 = [1, 2, 3, 4, 5]
 input_2 = [5, 2, 3, 4, 1]
@@ -8,8 +17,8 @@ input_3 = [1, 2, 5, 4, 3]
 input_4 = [3, 5]
 input_5 = [5, 3]
 
-assert find_largest(input_1, 0) == 4
-assert find_largest(input_2, 0) == 4
-assert find_largest(input_3, 0) == 4
-assert find_largest(input_4, 0) == 3
-assert find_largest(input_5, 0) == 3
+assert find_second_largest(input_1) == 4
+assert find_second_largest(input_2) == 4
+assert find_second_largest(input_3) == 4
+assert find_second_largest(input_4) == 3
+assert find_second_largest(input_5) == 3
