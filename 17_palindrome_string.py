f364bbb6e5586a78b88e60cc11af42bda9ad8321
Vivek <vivek.ratnaparkhi10@gmail.com>
2019-04-27 13:40:41 +0530

    check palindrome string

--- 17_palindrome_string.py
+++ 17_palindrome_string.py
@@ -16,3 +16,14 @@ assert check_palindrome('abc') == False
 assert check_palindrome('aba') == True
 assert check_palindrome('abba') == True
 assert check_palindrome('nitin') == True
+
+# The same can be easily acheived using Python's builtin function like below.
+
+def check_palindrome_easy(s):
+    assert len(s) >= 3
+    return s == s[::-1]
+
+assert check_palindrome('abc') == False
+assert check_palindrome('aba') == True
+assert check_palindrome('abba') == True
+assert check_palindrome('nitin') == True
